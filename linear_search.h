2702a4fbca333dbcdd056ca70e7fdae368fde544
Алексей Ершков <ershkov.alex2000@gmail.com>
2019-10-20 16:51:17 +0300

    linear_search done

---
+++ linear_search.h
@@ -0,0 +1,18 @@
+//
+// Created by Алексей on 2019-10-20.
+//
+
+#ifndef IZ2_LINEAR_SEARCH_H
+#define IZ2_LINEAR_SEARCH_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+#include <stdio.h>
+#define MB100 104857600 // size of 100 MB in Bytes
+
+double linear_search(char* str);
+
+
+#endif //IZ2_LINEAR_SEARCH_H
--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include "linear_search.h"
+
 
 int main() {
-    printf("Hello, World!\n");
+    char str[10] = "abc'A''Af'";
+    double output  = linear_search(str);
+    printf("%lf", output);
     return 0;
 }
